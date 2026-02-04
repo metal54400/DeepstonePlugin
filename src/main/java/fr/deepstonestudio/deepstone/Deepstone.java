9113702ee0ae79e266628e012fa9102c39adf41a
metal54400 <audecillier@hotmail.fr>
2026-02-04 21:16:58 +0100

    Deepstone plugin

---
+++ src/main/java/fr/deepstonestudio/deepstone/Deepstone.java
@@ -0,0 +1,17 @@
+package fr.deepstonestudio.deepstone;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Deepstone extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
