69cda86dd23f293639dbd69d3b73f8942cb86118
metal54400 <audecillier@hotmail.fr>
2026-02-19 23:25:02 +0100

    Correctif de cetains bug, quelque nouveauté
    Signed-off-by: metal54400 <audecillier@hotmail.fr>

--- src/main/java/fr/deepstonestudio/deepstone/util/Msg.java
+++ src/main/java/fr/deepstonestudio/deepstone/util/Msg.java
@@ -1,28 +1,30 @@
 package fr.deepstonestudio.deepstone.util;
 
-
-import net.kyori.adventure.text.TextComponent;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
-import org.jetbrains.annotations.NotNull;
-
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 
 public class Msg {
+
+    private Msg() {}
 
     public static String color(String s) {
         return ChatColor.translateAlternateColorCodes('&', s);
     }
 
-    public static void broadcast(String msg) {
-        Bukkit.getOnlinePlayers().forEach(p -> p.sendMessage(msg));
-        Bukkit.getConsoleSender().sendMessage(msg);
+    public static void send(Player player, String msg) {
+        player.sendMessage(color(msg));
     }
 
-    private Msg() {}
+    public static void broadcast(String msg) {
+        String colored = color(msg);
+        Bukkit.getOnlinePlayers().forEach(p -> p.sendMessage(colored));
+        Bukkit.getConsoleSender().sendMessage(colored);
+    }
 
-    public static Component  ok(String s) {
+    public static Component ok(String s) {
         return Component.text("§7[§e?§7] " + s, NamedTextColor.GREEN);
     }
 
@@ -33,6 +35,4 @@ public class Msg {
     public static Component info(String s) {
         return Component.text("§7[§d!§7] " + s, NamedTextColor.AQUA);
     }
-
-
 }
